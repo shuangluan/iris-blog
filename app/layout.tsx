1755317efa9e048d69b1dda3ea8cf5a978f201ee
Iris Luan <441385807@qq.com>
2026-08-13 22:20:14 -0400

    Refactor Plausible Analytics script integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,22 +61,14 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
         {plausibleDomain ? (
-          <>
-            <Script
-              async
-              src="https://plausible.io/js/pa-XK2kJd2nFeJH_dVYfpkbX.js"
-              strategy="afterInteractive"
-            />
-            <Script
-              id="plausible-init"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()`,
-              }}
-            />
-          </>
+          <Script
+            defer
+            data-domain={plausibleDomain}
+            src="https://plausible.io/js/script.js"
+            strategy="afterInteractive"
+          />
         ) : null}
       </body>
     </html>
   );
-}
+} 
